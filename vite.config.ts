2194925ebf8067c87ce407f20f6fcef968844087
amans199 <ahmed.ouda1997@gmail.com>
2024-12-15 11:06:24 +0100

    FEAT(ViteConfig): update base name in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/SDLC-lecture-presentation/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
